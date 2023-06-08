5e6eca107399489cc8018a22633395a71d90ce29
Aintomid <richardakintomide22@gmail.com>
2023-06-08 10:07:24 -0700

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-        a = int(sys.argv[1])
-        operator = sys.argv[2]
-        b = int(sys.argv[3])
+    count = len(argv)
 
-        result = None
+    if count != 4:
+        print("Usage: {} <a> <operator> <b>".format(a, b, argv[0]))
+        exit(1)
+        a = int(argv[1])
+        operator = argv[2]
+        b = int(argv[3])
 
-        if operator == '+':
-            result = add(a, b)
-        elif operator == '-':
-            result = sub(a, b)
-        elif operator == '*':
-            result = mul(a, b)
-        elif operator == '/':
-            result = div(a, b)
-        else:
+        def invalid_operator():
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-        print(f"{a} {operator} {b} = {result}")
+            exit(1)
+
+        def addition():
+            c = add(a, b)
+            print("{} + {} = {}".format(a, b, c))
+            return c
+
+        def subtraction():
+            d = sub(a, b)
+            print("{} - {} = {}".format(a, b, d))
+            return d
+
+        def multiplication():
+            e = mul(a, b)
+            print("{} * {} = {}".format(a, b, e))
+            return e
+
+        def division():
+            f = div(a, b)
+            print("{} / {} = {}".format(a, b, f))
+            return f
+
+        options = {
+                "+": addition,
+                "-": subtraction,
+                "*": multiplication,
+                "/": division
+        }
+        options.get(operator, invalid_operator)()
