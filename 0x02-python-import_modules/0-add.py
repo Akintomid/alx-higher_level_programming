da4f9b87eb674eb264d5058de043c6e1eb99bbea
Aintomid <richardakintomide22@gmail.com>
2023-06-08 05:46:38 -0700

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add as ab
-a = 1
-b = 2
-
-add_code = """
-def add(a, b):
-    return a + b
-"""
-exec(add_code)
-
-output = ab(a, b)
-print("{} + {} = {}".format(a, b, output))
+if __name__ == "__main__":
+    from add_0 import add as ab
+    a = 1
+    b = 2
+    output = ab(a, b)
+    print("{} + {} = {}".format(a, b, output))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
+from calculator_1 import add
+from calculator_1 import sub
+from calculator_1 import mul
+from calculator_1 import div
 
 a = 10
 b = 5
 
-from calculator_1 import add
-
 output = add(a, b)
 print("{} + {} = {}".format(a, b, output))
 
-from calculator_1 import sub
 
 output = sub(a, b)
 print("{} - {} = {}".format(a, b, output))
 
-from calculator_1 import mul
-
 output = mul(a, b)
 
 print("{} * {} = {}".format(a, b, output))
-
-from calculator_1 import div
 
 output = div(a, b)
 
